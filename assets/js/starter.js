95e724f4ac0ec055c75d143a78cf24f618bc78d2
Vojtech Sedlak <vojtech@linden.local>
2024-04-08 13:39:18 -0700

    js

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -11,17 +11,6 @@
 // import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
-
-
 // Countdown
 
 // Set the date we're counting down to
